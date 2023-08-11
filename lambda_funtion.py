a907f2708203f2a9864bb72c24cf22165c2cad63
mnafees88 <58204448+mnafees88@users.noreply.github.com>
2023-08-11 16:13:58 +0500

    new

---
+++ lambda_funtion.py
@@ -0,0 +1,8 @@
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!-test- this is testing-123-toiufsdfjhdsjkfhsdjkfhsdjkhfskdjfhsadjodsfdskljfdsl;fjdsfjds;lfkds;lfkl;23423432u74732987483247932472934kfhsdajkfhsadkjfhsdjkfh73r632478657856374256234593287seef-testing123456')
+    }
