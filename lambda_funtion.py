d5f19c54801527422ab38dba23613dbeb7b9ab74
mnafees88 <58204448+mnafees88@users.noreply.github.com>
2023-08-11 16:17:50 +0500

    Update lambda_funtion.py
    3432

--- lambda_funtion.py
+++ lambda_funtion.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!-test- this is testing-123-toiufsdfjhdsjkfhsdjkfhsdjkhfskdjfhsadjodsfdskljfdsl;fjdsfjds;lfkds;lfkl;23423432u74732987483247932472934kfhsdajkfhsadkjfhsdjkfh73r632478657856374256234593287seef-testing123456')
+        'body': json.dumps('Hello from Lambda!-test- this is testing-123-toiufsdfjhdsjkfhsdjkfhsdjkhfskdjfhsadjodsfds122222222222222222222222222222222222222231233333333333333333333333333333333kljfdsl;fjdsfjds;lfkds;lfkl;23423432u74732987483247932472934kfhsdajkfhsadkjfhsdjkfh73r632478657856374256234593287seef-testing123456')
     }
